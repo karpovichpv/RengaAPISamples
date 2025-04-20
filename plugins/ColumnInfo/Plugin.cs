ecd5215d3322eea95b2d2e284372236d60e2c110
karpovichpv <karpovich.pavel@gmail.com>
2025-04-20 21:09:30 +0300

    Add the template for the ColumnInfo plugin

---
+++ plugins/ColumnInfo/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using Renga;
+
+namespace ColumnInfo
+{
+	public class Plugin : IPlugin
+	{
+		readonly List<ActionEventSource> _actionEventSources = [];
+
+		public bool Initialize(string pluginFolder)
+		{
+			IApplication app = new Application();
+			IUI ui = app.UI;
+
+			IUIPanelExtension panelExtension = ui.CreateUIPanelExtension();
+
+			panelExtension.AddToolButton(CreateAction(ui));
+			ui.AddExtensionToPrimaryPanel(panelExtension);
+			ui.AddExtensionToActionsPanel(panelExtension, ViewType.ViewType_View3D);
+
+			return true;
+		}
+
+
+		public void Stop()
+		{
+			foreach (ActionEventSource eventSource in _actionEventSources)
+				eventSource.Dispose();
+
+			_actionEventSources.Clear();
+		}
+
+		private IAction CreateAction(IUI ui)
+		{
+			IAction action = ui.CreateAction();
+
+			ActionEventSource eventSource = new(action);
+			eventSource.Triggered += (obj, args) =>
+			{
+				ui.ShowMessageBox(MessageIcon.MessageIcon_Info, "sample plugin", "sample plugin");
+			};
+
+			_actionEventSources.Add(eventSource);
+			return action;
+		}
+	}
+}
