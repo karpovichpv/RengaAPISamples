e9a1bac51c942fdc2743a653addbf8e7cddeafc7
karpovichpv <karpovich.pavel@gmail.com>
2025-04-22 21:05:49 +0300

    Add the project for the context menu plugin

---
+++ plugins/ContextMenu/ColumnInfoGetter.cs
@@ -0,0 +1,45 @@
+﻿using Renga;
+using System.Text;
+
+namespace ContextMenu
+{
+	internal class ColumnInfoGetter(IModelObject modelObject, IProject project)
+	{
+		private readonly IModelObject _modelObject = modelObject;
+		private readonly IProject _project = project;
+
+		public string Get()
+		{
+			if (_modelObject is not IColumnParams)
+				return "Object isn't a column";
+
+			StringBuilder builder = new();
+			if (_modelObject is IColumnParams parameters)
+			{
+				AddParams(builder, parameters);
+				AddStyleParams(builder, parameters);
+			}
+
+			return builder.ToString();
+		}
+
+		private void AddStyleParams(StringBuilder builder, IColumnParams parameters)
+		{
+			IColumnStyleManager columnStyleManager = _project.ColumnStyleManager;
+			IColumnStyle style = columnStyleManager.GetColumnStyle(parameters.StyleId);
+
+			builder.AppendLine($"Style Id:{style.Id}");
+			builder.AppendLine($"Style Name:{style.Name}");
+			builder.AppendLine($"Style Profile:{style.Profile}");
+		}
+
+		private static void AddParams(StringBuilder builder, IColumnParams parameters)
+		{
+			builder.AppendLine($"Height:{parameters.Height}");
+			builder.AppendLine($"Position:{parameters.Position}");
+			builder.AppendLine($"StyleId:{parameters.StyleId}");
+			builder.AppendLine($"VerticalOffset:{parameters.VerticalOffset}");
+			builder.AppendLine($"Placement:{parameters.GetProfilePlacement()}");
+		}
+	}
+}
---
+++ plugins/ContextMenu/Plugin.cs
@@ -0,0 +1,87 @@
+﻿using Renga;
+using System.Text;
+
+namespace ContextMenu
+{
+	public class Plugin : IPlugin
+	{
+		private readonly List<ActionEventSource> _actionEventSources = [];
+		private IApplication? _app;
+
+		public bool Initialize(string pluginFolder)
+		{
+			IApplication app = new Application();
+			_app = app;
+			IUI ui = app.UI;
+
+			IUIPanelExtension panelExtension = ui.CreateUIPanelExtension();
+
+			panelExtension.AddToolButton(CreateAction(ui));
+			ui.AddExtensionToPrimaryPanel(panelExtension);
+			ui.AddExtensionToActionsPanel(panelExtension, ViewType.ViewType_View3D);
+
+			return true;
+		}
+
+
+		public void Stop()
+		{
+			foreach (ActionEventSource eventSource in _actionEventSources)
+				eventSource.Dispose();
+
+			_actionEventSources.Clear();
+		}
+
+		private IAction CreateAction(IUI ui)
+		{
+			IAction action = ui.CreateAction();
+
+			ActionEventSource eventSource = new(action);
+			eventSource.Triggered += (obj, args) =>
+			{
+				string columnInfo = GetColumnInfo();
+				ui.ShowMessageBox(MessageIcon.MessageIcon_Info, "sample plugin", columnInfo);
+			};
+
+			_actionEventSources.Add(eventSource);
+			return action;
+		}
+
+		private string GetColumnInfo()
+		{
+			IProject? project = null;
+			ISelection? selection = null;
+			if (_app is not null)
+			{
+				project = _app.Project;
+				selection = _app.Selection;
+			}
+
+			if (project is null)
+				return $"Project is null";
+
+			if (selection is null)
+			{
+				return "Nothing was selected";
+			}
+
+			Array array = selection.GetSelectedObjects();
+			StringBuilder builder = new();
+			if (array.Length != 0)
+			{
+				object? obj = array.GetValue(0);
+
+				if (obj is int id)
+				{
+					IModel model = project.Model;
+					IModelObject modelObject = model.GetObjects().GetById(id);
+
+					ColumnInfoGetter getter = new(modelObject, project);
+					return getter.Get();
+				}
+			}
+
+			return builder.ToString();
+		}
+	}
+}
