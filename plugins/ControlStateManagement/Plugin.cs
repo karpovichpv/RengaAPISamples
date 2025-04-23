7e37258253645a1f7958d167a1069c91860d76ad
karpovichpv <karpovich.pavel@gmail.com>
2025-04-23 21:02:27 +0300

    Add the project for the ControlStateManagement

---
+++ plugins/ControlStateManagement/Plugin.cs
@@ -0,0 +1,17 @@
+﻿using Renga;
+
+namespace ControlStateManagement
+{
+	public class Plugin : IPlugin
+	{
+		public bool Initialize(string pluginFolder)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Stop()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
