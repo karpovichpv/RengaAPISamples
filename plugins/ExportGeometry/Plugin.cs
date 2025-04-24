7d62b873e5e5dc7473e49983783cb45168b89bcf
karpovichpv <karpovich.pavel@gmail.com>
2025-04-24 21:42:16 +0300

    Add a button on the panel

--- plugins/ExportGeometry/Plugin.cs
+++ plugins/ExportGeometry/Plugin.cs
@@ -4,14 +4,38 @@ namespace ExportGeometry
 {
 	public class Plugin : IPlugin
 	{
+		private Application? _app;
+		private IUI? _ui;
+		private ActionEventSource? _eventSource;
+
 		public bool Initialize(string pluginFolder)
 		{
+			_app = new Application();
+			_ui = _app.UI;
+
+			IUIPanelExtension panelExtension = _ui.CreateUIPanelExtension();
+			IAction exportObjects = _ui.CreateAction();
+			exportObjects.ToolTip = "ExportObjects";
+			_eventSource = new ActionEventSource(exportObjects);
+			_eventSource.Triggered += OnExportObjectsButtonClicked;
+			panelExtension.AddToolButton(exportObjects);
+
+			_ui.AddExtensionToPrimaryPanel(panelExtension);
+
 			return true;
+		}
+
+		private void OnExportObjectsButtonClicked(object? sender, EventArgs e)
+		{
+			_ui?.ShowMessageBox(
+				MessageIcon.MessageIcon_Info,
+				"Export objects",
+				"Exported objects");
 		}
 
 		public void Stop()
 		{
-
+			_eventSource?.Dispose();
 		}
 	}
 }
