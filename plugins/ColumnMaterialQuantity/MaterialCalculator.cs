d5a88ecb6ef122780c1511efcae5310046e8e4a9
karpovichpv <karpovich.pavel@gmail.com>
2025-04-26 11:35:23 +0300

    Add an example for calculation quantities of column

---
+++ plugins/ColumnMaterialQuantity/MaterialCalculator.cs
@@ -0,0 +1,47 @@
+﻿using Renga;
+using System.Text;
+
+namespace ColumnMaterialQuantity
+{
+	internal class MaterialCalculator
+	{
+		private readonly IModelObject? _column;
+
+		public MaterialCalculator(IModelObject? column)
+		{
+			_column = column;
+		}
+
+		internal string Calculate()
+		{
+			StringBuilder builder = new();
+			if (_column is not null)
+			{
+				IQuantityContainer quantities = _column.GetQuantities();
+
+				builder.AppendLine(GetAreaString(quantities));
+				builder.AppendLine(GetCrossSectionAreaString(quantities));
+
+				return builder.ToString();
+			}
+
+			return "Не возможно посчить какие-либо характеристики для объекта";
+		}
+
+		private static string GetAreaString(IQuantityContainer quantities)
+		{
+			IQuantity area = quantities.Get(QuantityIds.Area);
+			return area is not null
+				? $"Area {area.AsArea(AreaUnit.AreaUnit_Centimeters2)} cm2"
+				: "Не возможно получить площадь";
+		}
+
+		private static string GetCrossSectionAreaString(IQuantityContainer quantities)
+		{
+			IQuantity area = quantities.Get(QuantityIds.NetCrossSectionArea);
+			return area is not null
+				? $"Площадь поперечного сечения {area.AsArea(AreaUnit.AreaUnit_Centimeters2)} см2"
+				: "Не возможно получить площадь поперечного сечения";
+		}
+	}
+}
---
+++ plugins/ColumnMaterialQuantity/Plugin.cs
@@ -0,0 +1,79 @@
+﻿using Renga;
+
+namespace ColumnMaterialQuantity
+{
+	public class Plugin : IPlugin
+	{
+		private Application? _app;
+		private ISelection? _selection;
+		private IUI? _ui;
+		private ActionEventSource? _eventSource;
+
+		public bool Initialize(string pluginFolder)
+		{
+			_app = new Application();
+			_ui = _app.UI;
+
+			IUIPanelExtension panelExtension = _ui.CreateUIPanelExtension();
+			IAction exportObjects = _ui.CreateAction();
+			exportObjects.ToolTip = "Column Material Quantity";
+			_eventSource = new ActionEventSource(exportObjects);
+			_eventSource.Triggered += OnExportObjectsButtonClicked;
+			panelExtension.AddToolButton(exportObjects);
+
+			_ui.AddExtensionToPrimaryPanel(panelExtension);
+
+			return true;
+		}
+
+
+		public void Stop()
+		{
+			_eventSource?.Dispose();
+		}
+
+		private void OnExportObjectsButtonClicked(object? sender, EventArgs e)
+		{
+			IModelObject? column = GetSelectedColumn();
+			MaterialCalculator exporter = new(column);
+			string exportString = exporter.Calculate();
+			_ui?.ShowMessageBox(
+				MessageIcon.MessageIcon_Info,
+				"Materials was exported",
+				exportString);
+		}
+
+		private IModelObject? GetSelectedColumn()
+		{
+			IModel? model = null;
+			if (_app is not null)
+			{
+				model = _app.Project.Model;
+				_selection = _app.Selection;
+			}
+
+			IModelObjectCollection? modelObjects = null;
+			if (model is not null)
+			{
+				modelObjects = model.GetObjects();
+			}
+
+			if (_selection is not null && modelObjects is not null)
+			{
+				Array objects = _selection.GetSelectedObjects();
+				for (int i = 0; i < objects.Length; i++)
+				{
+					object? currentId = objects.GetValue(i);
+					if (currentId is int id)
+					{
+						IModelObject? obj = modelObjects?.GetById(id);
+						if (obj is IColumnParams)
+							return obj;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+}
