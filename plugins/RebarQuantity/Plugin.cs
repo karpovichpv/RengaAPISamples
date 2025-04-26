96024e45cce656d4d38bd970dc2ff0c61b0ba674
karpovichpv <karpovich.pavel@gmail.com>
2025-04-26 17:44:32 +0300

    Add the rebar quantity example

---
+++ plugins/RebarQuantity/Plugin.cs
@@ -0,0 +1,76 @@
+﻿using Renga;
+
+namespace RebarQuantity
+{
+	public class Plugin : IPlugin
+	{
+		private Application? _app;
+		private ISelection? _selection;
+		private IUI? _ui;
+		private ActionEventSource? _eventSource;
+
+		public bool Initialize(string pluginFolder)
+		{
+			_app = new Application();
+			_ui = _app.UI;
+
+			IUIPanelExtension panelExtension = _ui.CreateUIPanelExtension();
+			IAction exportObjects = _ui.CreateAction();
+			exportObjects.ToolTip = "Rebar Quantity";
+			_eventSource = new ActionEventSource(exportObjects);
+			_eventSource.Triggered += OnExportObjectsButtonClicked;
+			panelExtension.AddToolButton(exportObjects);
+
+			_ui.AddExtensionToPrimaryPanel(panelExtension);
+
+			return true;
+		}
+
+
+		public void Stop()
+		{
+			_eventSource?.Dispose();
+		}
+
+		private void OnExportObjectsButtonClicked(object? sender, EventArgs e)
+		{
+			IModelObject? column = GetSelectedObject();
+			RebarQuantityGetter getter = new(column);
+			string exportString = getter.Get();
+			_ui?.ShowMessageBox(
+				MessageIcon.MessageIcon_Info,
+				"Rebar",
+				exportString);
+		}
+
+		private IModelObject? GetSelectedObject()
+		{
+			IModel? model = null;
+			if (_app is not null)
+			{
+				model = _app.Project.Model;
+				_selection = _app.Selection;
+			}
+
+			IModelObjectCollection? modelObjects = null;
+			if (model is not null)
+				modelObjects = model.GetObjects();
+
+			if (_selection is not null && modelObjects is not null)
+			{
+				int[] objects = (int[])_selection.GetSelectedObjects();
+				if (objects.Length != 0)
+				{
+					object? currentId = objects.GetValue(0);
+					if (currentId is int id)
+					{
+						IModelObject? obj = modelObjects?.GetById(id);
+						return obj;
+					}
+				}
+			}
+
+			return null;
+		}
+	}
+}
---
+++ plugins/RebarQuantity/RebarQuantityGetter.cs
@@ -0,0 +1,95 @@
+﻿using Renga;
+using System.Text;
+
+namespace RebarQuantity
+{
+	internal class RebarQuantityGetter
+	{
+		private readonly IModelObject? _object;
+		private readonly IReinforcementUnitStyleManager? _reinforcementStyleManager;
+
+		public RebarQuantityGetter(IModelObject? column)
+		{
+			_object = column;
+			IApplication app = new Application();
+
+			if (app is not null)
+			{
+				IProject project = app.Project;
+				if (project is not null)
+				{
+					_reinforcementStyleManager = project.ReinforcementUnitStyleManager;
+				}
+			}
+
+		}
+
+		internal string Get()
+		{
+			StringBuilder builder = new();
+			if (_object is not null)
+			{
+				if (_object is IObjectReinforcementModel reinforcementObject)
+				{
+					builder.Append(GetUsages(reinforcementObject));
+				}
+
+
+				return builder.ToString();
+			}
+
+			return "Не возможно посчить какие-либо характеристики для объекта";
+		}
+
+		private string GetUsages(IObjectReinforcementModel reinforcementObject)
+		{
+			StringBuilder builder = new();
+			IRebarUsageCollection usages = reinforcementObject.GetRebarUsages();
+			for (int i = 0; i < usages.Count; i++)
+			{
+				IRebarUsage current = usages.Get(i);
+				IQuantityContainer quantities = current.GetQuantities();
+				builder.AppendLine("-----");
+				builder.AppendLine(GetStyleProperties(current));
+				builder.AppendLine(GetLengthString(quantities));
+				builder.AppendLine(GetMassString(quantities));
+			}
+
+			return builder.ToString();
+		}
+
+		private string GetStyleProperties(IRebarUsage current)
+		{
+			if (_reinforcementStyleManager is not null)
+			{
+
+				IRebarStyle rebarStyle = _reinforcementStyleManager.GetRebarStyle(current.StyleId);
+				if (rebarStyle is not null)
+				{
+					return
+						$"Diameter: {rebarStyle.Diameter:f2}; " +
+						$"name: {rebarStyle.Name}; " +
+						$"grade name: {rebarStyle.GradeName}; " +
+						$"tensileStrength {rebarStyle.GradeTensileStrength}";
+				}
+			}
+			return "Cannot obtain the rebar style parameters parameters";
+		}
+
+		private static string GetLengthString(IQuantityContainer quantities)
+		{
+			IQuantity length = quantities.Get(QuantityIds.TotalRebarLength);
+			return length is not null
+				? $"Length: {length.AsLength(LengthUnit.LengthUnit_Millimeters)} cm"
+				: "Не возможно получить длину";
+		}
+
+		private static string GetMassString(IQuantityContainer quantities)
+		{
+			IQuantity value = quantities.Get(QuantityIds.TotalRebarMass);
+			return value is not null
+				? $"Mass: {value.AsMass(MassUnit.MassUnit_Kilograms)} kg"
+				: "Не возможно получить массу";
+		}
+	}
+}
