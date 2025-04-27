678f8cecaeefef05f84da170c519f9e1c141e401
karpovichpv <karpovich.pavel@gmail.com>
2025-04-27 21:39:08 +0300

    Clean the project

--- plugins/PropertiesAndParameters/Plugin.cs
+++ plugins/PropertiesAndParameters/Plugin.cs
@@ -20,22 +20,7 @@ namespace ObjectPlacementModifying
 			_contextMenu = contextMenu;
 
 			IContextMenuNodeItem nodeItem = contextMenu.AddNodeItem();
-			nodeItem.DisplayName = "Placement editing";
-			nodeItem.AddActionItem(CreateAction(ui, "X+" + _movementValue, (IPlacement3D placement) =>
-			  {
-				  placement.Move(new Vector3D { X = _movementValue, Y = 0, Z = 0 });
-				  return placement;
-			  }));
-			nodeItem.AddActionItem(CreateAction(ui, "Y+" + _movementValue, (IPlacement3D placement) =>
-			  {
-				  placement.Move(new Vector3D { X = 0, Y = _movementValue, Z = 0 });
-				  return placement;
-			  }));
-			nodeItem.AddActionItem(CreateAction(ui, "Z+" + _movementValue, (IPlacement3D placement) =>
-			  {
-				  placement.Move(new Vector3D { X = 0, Y = 0, Z = _movementValue });
-				  return placement;
-			  }));
+			nodeItem.DisplayName = "Properties and parameters";
 
 			_ui?.AddContextMenu(
 				new Guid(),
@@ -46,37 +31,6 @@ namespace ObjectPlacementModifying
 			return true;
 		}
 
-		private IAction CreateAction(IUI ui, string displayName, Func<IPlacement3D, IPlacement3D> func)
-		{
-			var action = ui.CreateAction();
-			action.DisplayName = displayName;
-
-			var events = new Renga.ActionEventSource(action);
-			events.Triggered += (s, e) =>
-			{
-				if (_app is not null)
-				{
-					int[] selectedObjectsIds = (int[])_app.Selection.GetSelectedObjects();
-					if (selectedObjectsIds.Length > 1)
-						return;
-
-					int id = selectedObjectsIds.ElementAt(0);
-					if (_app.Project.Model.GetObjects().GetById(id) is not ILevelObject levelObject)
-						return;
-
-					IOperation op = _app.Project.CreateOperation();
-					op.Start();
-					IPlacement3D placement = levelObject.GetPlacement();
-					placement = func(placement);
-					levelObject.SetPlacement(placement);
-					op.Apply();
-				}
-			};
-
-			_eventSources.Add(events);
-
-			return action;
-		}
 
 		public void Stop()
 		{
