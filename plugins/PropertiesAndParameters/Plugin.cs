a8783cce2cc5ca0ca351db4d7b31a6f8f9611c86
karpovichpv <karpovich.pavel@gmail.com>
2025-04-27 22:33:53 +0300

    Refactor

--- plugins/PropertiesAndParameters/Plugin.cs
+++ plugins/PropertiesAndParameters/Plugin.cs
@@ -1,5 +1,4 @@
 ﻿using Renga;
-using System.Text;
 
 namespace PropertiesAndParameters
 {
@@ -7,47 +6,52 @@ namespace PropertiesAndParameters
 	{
 		private readonly List<ActionEventSource> _eventSources = [];
 		private Application? _app;
+		private IUI? _ui;
+		private IUIPanelExtension? _panelExtension;
 
 		public bool Initialize(string pluginFolder)
 		{
 			_app = new Application();
 
-			IUI ui = _app.UI;
-			IUIPanelExtension panelExtension = ui.CreateUIPanelExtension();
-
-			Action<string, string, Func<string>> createMessageBox =
-				(tooltip, title, callBack) =>
-				{
-					var action = ui.CreateAction();
-					action.ToolTip = tooltip;
-					panelExtension.AddToolButton(action);
-
-					ActionEventSource events = new(action);
-					events.Triggered += (sender, arguments) =>
-					{
-						ui.ShowMessageBox(
-							MessageIcon.MessageIcon_Info,
-							title,
-							callBack?.Invoke());
-					};
-				};
+			_ui = _app.UI;
+			_panelExtension = _ui.CreateUIPanelExtension();
 
 
-			createMessageBox(
+			CreateMessageBox(
 				"Add properties to all floors in the model",
 				"Adding properties to all floors",
-				AddPropertyToAllFloors
-				);
+				PluginHelpers.AddPropertyToAllFloors);
 
-			createMessageBox(
+			CreateMessageBox(
 				"Printing the first floor properties values",
 				"Print properties",
-				PrintPropertyParameters
-				);
+				PluginHelpers.PrintPropertyParameters);
 
-			ui.AddExtensionToPrimaryPanel(panelExtension);
+			_ui.AddExtensionToPrimaryPanel(_panelExtension);
 
 			return true;
+		}
+
+		private void CreateMessageBox(
+			string tooltip,
+			string title,
+			Func<IApplication, string> callBack)
+		{
+			if (_ui is not null && _app is not null)
+			{
+				var action = _ui.CreateAction();
+				action.ToolTip = tooltip;
+				_panelExtension?.AddToolButton(action);
+
+				ActionEventSource events = new(action);
+				events.Triggered += (sender, arguments) =>
+				{
+					_ui.ShowMessageBox(
+						MessageIcon.MessageIcon_Info,
+						title,
+						callBack?.Invoke(_app));
+				};
+			}
 		}
 
 		public void Stop()
@@ -56,112 +60,6 @@ namespace PropertiesAndParameters
 				eventSource.Dispose();
 
 			_eventSources.Clear();
-		}
-
-		private string AddPropertyToAllFloors()
-		{
-			IProject? project = null;
-			if (_app is not null)
-				project = _app.Project;
-
-			if (project is not null)
-			{
-				if (project.HasActiveOperation())
-					return "It's impossible to modify the project. Another operation in progress";
-
-				IOperation operation = project.CreateOperation();
-				operation.Start();
-
-				IPropertyManager propertyManager = project.PropertyManager;
-				PropertyDescription propertyDescription = new()
-				{
-					Name = "New property for floor",
-					Type = PropertyType.PropertyType_String
-				};
-
-				Guid attributeGuid = Guid.NewGuid();
-				propertyManager.RegisterProperty(attributeGuid, propertyDescription);
-				propertyManager.AssignPropertyToType(attributeGuid, ObjectTypes.Floor);
-
-				IModel model = project.Model;
-				IModelObjectCollection objects = model.GetObjects();
-
-				for (int i = 0; i < objects.Count; i++)
-				{
-					IModelObject obj = objects.GetByIndex(i);
-					if (obj.ObjectType == ObjectTypes.Floor)
-					{
-						var property = obj.GetProperties().Get(attributeGuid);
-						property.SetStringValue("Test floor parameter");
-					}
-				}
-
-				operation.Apply();
-			}
-
-			return "Operation was successfully applied";
-		}
-
-		private string PrintPropertyParameters()
-		{
-
-			IProject? project = null;
-			if (_app is not null)
-				project = _app.Project;
-
-			if (project is not null)
-			{
-				IModel model = project.Model;
-				IModelObjectCollection objects = model.GetObjects();
-
-				for (int i = 0; i < objects.Count; i++)
-				{
-					IModelObject obj = objects.GetByIndex(i);
-					if (obj.ObjectType == ObjectTypes.Floor)
-					{
-						IPropertyContainer container = obj.GetProperties();
-						IGuidCollection guids = container.GetIds();
-
-						StringBuilder builder = new("Floor properties");
-						for (int j = 0; j < guids.Count; j++)
-						{
-							Guid current = guids.Get(j);
-							IProperty property = container.Get(current);
-							builder.AppendLine("");
-							builder.Append($"Name: {property.Name}");
-							builder.Append($"Type: {property.Type}");
-							builder.Append($"Value: {GetPropertyValue(property)}");
-						}
-						return builder.ToString();
-					}
-				}
-			}
-
-			return "Operation was successfully applied";
-
-		}
-
-		private static object? GetPropertyValue(IProperty property)
-		{
-			switch (property.Type)
-			{
-				case Renga.PropertyType.PropertyType_Angle: return property.GetAngleValue(Renga.AngleUnit.AngleUnit_Degrees);
-				case Renga.PropertyType.PropertyType_Double: return property.GetDoubleValue();
-				case Renga.PropertyType.PropertyType_String: return property.GetStringValue();
-				case Renga.PropertyType.PropertyType_Area: return property.GetAreaValue(Renga.AreaUnit.AreaUnit_Meters2);
-				case Renga.PropertyType.PropertyType_Boolean: return property.GetBooleanValue();
-				case Renga.PropertyType.PropertyType_Enumeration: return property.GetEnumerationValue();
-				case Renga.PropertyType.PropertyType_Integer: return property.GetIntegerValue();
-				case Renga.PropertyType.PropertyType_Length: return property.GetLengthValue(Renga.LengthUnit.LengthUnit_Meters);
-				case Renga.PropertyType.PropertyType_Logical: return property.GetLogicalValue();
-				case Renga.PropertyType.PropertyType_Mass: return property.GetMassValue(Renga.MassUnit.MassUnit_Kilograms);
-				case Renga.PropertyType.PropertyType_Volume: return property.GetVolumeValue(Renga.VolumeUnit.VolumeUnit_Meters3);
-				case PropertyType.PropertyType_Undefined:
-					break;
-				default:
-					break;
-			}
-			return null;
 		}
 	}
 }
---
+++ plugins/PropertiesAndParameters/PluginHelpers.cs
@@ -0,0 +1,111 @@
+﻿using Renga;
+using System.Text;
+
+internal static class PluginHelpers
+{
+	public static string AddPropertyToAllFloors(IApplication app)
+	{
+		IProject? project = null;
+		if (app is not null)
+			project = app.Project;
+
+		if (project is not null)
+		{
+			if (project.HasActiveOperation())
+				return "It's impossible to modify the project. Another operation in progress";
+
+			IOperation operation = project.CreateOperation();
+			operation.Start();
+
+			IPropertyManager propertyManager = project.PropertyManager;
+			PropertyDescription propertyDescription = new()
+			{
+				Name = "New property for floor",
+				Type = PropertyType.PropertyType_String
+			};
+
+			Guid attributeGuid = Guid.NewGuid();
+			propertyManager.RegisterProperty(attributeGuid, propertyDescription);
+			propertyManager.AssignPropertyToType(attributeGuid, ObjectTypes.Floor);
+
+			IModel model = project.Model;
+			IModelObjectCollection objects = model.GetObjects();
+
+			for (int i = 0; i < objects.Count; i++)
+			{
+				IModelObject obj = objects.GetByIndex(i);
+				if (obj.ObjectType == ObjectTypes.Floor)
+				{
+					var property = obj.GetProperties().Get(attributeGuid);
+					property.SetStringValue("Test floor parameter");
+				}
+			}
+
+			operation.Apply();
+		}
+
+		return "Operation was successfully applied";
+	}
+
+	public static object? GetPropertyValue(IProperty property)
+	{
+		switch (property.Type)
+		{
+			case PropertyType.PropertyType_Angle: return property.GetAngleValue(Renga.AngleUnit.AngleUnit_Degrees);
+			case PropertyType.PropertyType_Double: return property.GetDoubleValue();
+			case PropertyType.PropertyType_String: return property.GetStringValue();
+			case PropertyType.PropertyType_Area: return property.GetAreaValue(Renga.AreaUnit.AreaUnit_Meters2);
+			case PropertyType.PropertyType_Boolean: return property.GetBooleanValue();
+			case PropertyType.PropertyType_Enumeration: return property.GetEnumerationValue();
+			case PropertyType.PropertyType_Integer: return property.GetIntegerValue();
+			case PropertyType.PropertyType_Length: return property.GetLengthValue(Renga.LengthUnit.LengthUnit_Meters);
+			case PropertyType.PropertyType_Logical: return property.GetLogicalValue();
+			case PropertyType.PropertyType_Mass: return property.GetMassValue(Renga.MassUnit.MassUnit_Kilograms);
+			case PropertyType.PropertyType_Volume: return property.GetVolumeValue(Renga.VolumeUnit.VolumeUnit_Meters3);
+			case PropertyType.PropertyType_Undefined:
+				break;
+			default:
+				break;
+		}
+		return null;
+	}
+
+	public static string PrintPropertyParameters(IApplication app)
+	{
+
+		IProject? project = null;
+		if (app is not null)
+			project = app.Project;
+
+		if (project is not null)
+		{
+			IModel model = project.Model;
+			IModelObjectCollection objects = model.GetObjects();
+
+			for (int i = 0; i < objects.Count; i++)
+			{
+				IModelObject obj = objects.GetByIndex(i);
+				if (obj.ObjectType == ObjectTypes.Floor)
+				{
+					IPropertyContainer container = obj.GetProperties();
+					IGuidCollection guids = container.GetIds();
+
+					StringBuilder builder = new("Floor properties");
+					for (int j = 0; j < guids.Count; j++)
+					{
+						Guid current = guids.Get(j);
+						IProperty property = container.Get(current);
+						builder.AppendLine("");
+						builder.Append($"Name: {property.Name}");
+						builder.Append($"Type: {property.Type}");
+						builder.Append($"Value: {GetPropertyValue(property)}");
+					}
+					return builder.ToString();
+				}
+			}
+		}
+
+		return "Operation was successfully applied";
+
+	}
+}
