3af11d5f44c569c31275173291cb11541bdb3f40
karpovichpv <karpovich.pavel@gmail.com>
2025-04-27 22:05:23 +0300

    Add a sample property for the floor object

--- plugins/PropertiesAndParameters/Plugin.cs
+++ plugins/PropertiesAndParameters/Plugin.cs
@@ -1,13 +1,10 @@
 ﻿using Renga;
 
-namespace ObjectPlacementModifying
+namespace PropertiesAndParameters
 {
 	public class Plugin : IPlugin
 	{
-		private const int _movementValue = 100;
 		private readonly List<ActionEventSource> _eventSources = [];
-		private IUI? _ui;
-		private IContextMenu? _contextMenu;
 		private Application? _app;
 
 		public bool Initialize(string pluginFolder)
@@ -15,22 +12,36 @@ namespace ObjectPlacementModifying
 			_app = new Application();
 
 			IUI ui = _app.UI;
-			_ui = ui;
-			IContextMenu contextMenu = ui.CreateContextMenu();
-			_contextMenu = contextMenu;
+			IUIPanelExtension panelExtension = ui.CreateUIPanelExtension();
 
-			IContextMenuNodeItem nodeItem = contextMenu.AddNodeItem();
-			nodeItem.DisplayName = "Properties and parameters";
+			Action<string, string, Func<string>> createMessageBox =
+				(tooltip, title, callBack) =>
+				{
+					var action = ui.CreateAction();
+					action.ToolTip = tooltip;
+					panelExtension.AddToolButton(action);
 
-			_ui?.AddContextMenu(
-				new Guid(),
-				_contextMenu,
-				ViewType.ViewType_View3D,
-				ContextMenuShowCase.ContextMenuShowCase_Selection);
+					ActionEventSource events = new(action);
+					events.Triggered += (sender, arguments) =>
+					{
+						ui.ShowMessageBox(
+							MessageIcon.MessageIcon_Info,
+							title,
+							callBack?.Invoke());
+					};
+				};
+
+
+			createMessageBox(
+				"Add properties to all floors in the model",
+				"Adding properties to all floors",
+				AddPropertyToAllFloors
+				);
+
+			ui.AddExtensionToPrimaryPanel(panelExtension);
 
 			return true;
 		}
-
 
 		public void Stop()
 		{
@@ -38,6 +49,50 @@ namespace ObjectPlacementModifying
 				eventSource.Dispose();
 
 			_eventSources.Clear();
+		}
+
+		private string AddPropertyToAllFloors()
+		{
+			IProject? project = null;
+			if (_app is not null)
+				project = _app.Project;
+
+			if (project is not null)
+			{
+				if (project.HasActiveOperation())
+					return "It's impossible to modify the project. Another operation in progress";
+
+				IOperation operation = project.CreateOperation();
+				operation.Start();
+
+				IPropertyManager propertyManager = project.PropertyManager;
+				PropertyDescription propertyDescription = new()
+				{
+					Name = "New property for floor",
+					Type = PropertyType.PropertyType_String
+				};
+
+				Guid attributeGuid = Guid.NewGuid();
+				propertyManager.RegisterProperty(attributeGuid, propertyDescription);
+				propertyManager.AssignPropertyToType(attributeGuid, ObjectTypes.Floor);
+
+				IModel model = project.Model;
+				IModelObjectCollection objects = model.GetObjects();
+
+				for (int i = 0; i < objects.Count; i++)
+				{
+					IModelObject obj = objects.GetByIndex(i);
+					if (obj.ObjectType == ObjectTypes.Floor)
+					{
+						var property = obj.GetProperties().Get(attributeGuid);
+						property.SetStringValue("Test floor parameter");
+					}
+				}
+
+				operation.Apply();
+			}
+
+			return "Operation was successfully applied";
 		}
 	}
 }
