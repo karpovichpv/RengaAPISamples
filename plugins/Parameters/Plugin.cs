70218d04bbf30860644d3881e9d83ca058909743
karpovichpv <karpovich.pavel@gmail.com>
2025-04-28 21:39:44 +0300

    Add reporting of plate style parameters

---
+++ plugins/Parameters/Plugin.cs
@@ -0,0 +1,60 @@
+﻿using Renga;
+
+namespace Parameters
+{
+	public class Plugin : IPlugin
+	{
+		private readonly List<ActionEventSource> _eventSources = [];
+		private Application? _app;
+		private IUI? _ui;
+		private IUIPanelExtension? _panelExtension;
+
+		public bool Initialize(string pluginFolder)
+		{
+			_app = new Application();
+
+			_ui = _app.UI;
+			_panelExtension = _ui.CreateUIPanelExtension();
+
+
+			CreateMessageBox(
+				"Plate styles parameters",
+				"Plate styles parameters",
+				PluginHelpers.ShowPlateStylesParameters);
+
+			_ui.AddExtensionToPrimaryPanel(_panelExtension);
+
+			return true;
+		}
+
+		private void CreateMessageBox(
+			string tooltip,
+			string title,
+			Func<IApplication, string> callBack)
+		{
+			if (_ui is not null && _app is not null)
+			{
+				var action = _ui.CreateAction();
+				action.ToolTip = tooltip;
+				_panelExtension?.AddToolButton(action);
+
+				ActionEventSource events = new(action);
+				events.Triggered += (sender, arguments) =>
+				{
+					_ui.ShowMessageBox(
+						MessageIcon.MessageIcon_Info,
+						title,
+						callBack?.Invoke(_app));
+				};
+			}
+		}
+
+		public void Stop()
+		{
+			foreach (ActionEventSource eventSource in _eventSources)
+				eventSource.Dispose();
+
+			_eventSources.Clear();
+		}
+	}
+}
---
+++ plugins/Parameters/PluginHelpers.cs
@@ -0,0 +1,49 @@
+﻿using Renga;
+using System.Text;
+
+internal static class PluginHelpers
+{
+	public static string ShowPlateStylesParameters(IApplication app)
+	{
+		StringBuilder builder = new();
+		builder.AppendLine("Plate style parameters:");
+		IProject project = app.Project;
+		IEntityCollection styles = project.PlateStyles;
+		foreach (int id in styles.GetIds())
+		{
+			IEntity style = styles.GetById(id);
+
+			if (style is IParameterContainer parameters)
+			{
+				builder.AppendLine($"{styles.GetById(id).Name}: {OutputParameters(parameters)}");
+			}
+		}
+
+		return builder.ToString();
+	}
+
+	private static string OutputParameters(IParameterContainer parameters)
+	{
+		IGuidCollection ids = parameters.GetIds();
+		StringBuilder builder = new();
+		for (int i = 0; i < ids.Count; i++)
+		{
+			IParameter parameter = parameters.Get(ids.Get(i));
+			builder.AppendLine($"{parameter.Definition.Text} {GetParameterValue(parameter)}");
+		}
+
+		return builder.ToString();
+	}
+
+	private static object? GetParameterValue(Renga.IParameter parameter)
+	{
+		return parameter.ValueType switch
+		{
+			ParameterValueType.ParameterValueType_Double => parameter.GetDoubleValue(),
+			ParameterValueType.ParameterValueType_String => parameter.GetStringValue(),
+			ParameterValueType.ParameterValueType_Int => parameter.GetIntValue(),
+			ParameterValueType.ParameterValueType_Bool => parameter.GetBoolValue(),
+			_ => null,
+		};
+	}
+}
