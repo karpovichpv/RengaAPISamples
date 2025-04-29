2c957d630a7f48582e75f16afee968fc390a0387
karpovichpv <karpovich.pavel@gmail.com>
2025-04-29 18:59:34 +0300

    Add the example for switching to the assembly view

--- plugins/ViewSwitching/Plugin.cs
+++ plugins/ViewSwitching/Plugin.cs
@@ -16,6 +16,7 @@ namespace ViewSwitching
 
 			IUIPanelExtension extension = ui.CreateUIPanelExtension();
 			extension.AddToolButton(CreateMainViewAction(ui));
+			extension.AddToolButton(CreateAssemblyViewAction(ui));
 			ui.AddExtensionToPrimaryPanel(extension);
 
 			return true;
@@ -41,6 +42,37 @@ namespace ViewSwitching
 				{
 					int buildingId = _app.Project.BuildingInfo.Id;
 					_app.OpenViewByEntity(buildingId);
+				}
+			};
+
+			_eventSources.Add(events);
+
+			return action;
+		}
+
+		private IAction CreateAssemblyViewAction(IUI ui)
+		{
+			IAction action = ui.CreateAction();
+			action.DisplayName = "AssemblyViewAction";
+
+			var events = new ActionEventSource(action);
+			events.Triggered += (sender, arguments) =>
+			{
+				if (_app is not null)
+				{
+					IEntityCollection assemblies = _app.Project.Assemblies;
+					if (assemblies.Count == 0)
+						return;
+
+					List<int> assemblyIds = [];
+					for (int i = 0; i < assemblies.Count; i++)
+					{
+						IEntity assembly = assemblies.GetByIndex(i);
+						assemblyIds.Add(assembly.Id);
+					}
+
+					List<int> groupedIds = assemblyIds.Distinct().ToList();
+					_app.OpenViewByEntity(groupedIds.First());
 				}
 			};
 
