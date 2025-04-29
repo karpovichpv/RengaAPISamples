f594b5d2d91d587a23fec752f4e6964bc5eb55da
karpovichpv <karpovich.pavel@gmail.com>
2025-04-29 13:14:21 +0300

    Add the project for switching view

---
+++ plugins/ViewSwitching/Plugin.cs
@@ -0,0 +1,20 @@
+﻿using Renga;
+
+namespace SystemEvents
+{
+	public class Plugin : IPlugin
+	{
+		public bool Initialize(string pluginFolder)
+		{
+			return true;
+		}
+
+		private void OnProjectSaved(string obj)
+		{
+		}
+
+		public void Stop()
+		{
+		}
+	}
+}
