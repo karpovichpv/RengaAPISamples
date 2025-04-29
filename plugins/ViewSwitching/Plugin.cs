cd63ccc18f089f422a16f4a9f62f980c32c1d1cd
karpovichpv <karpovich.pavel@gmail.com>
2025-04-29 13:26:55 +0300

    Add switch to the main view

--- plugins/ViewSwitching/Plugin.cs
+++ plugins/ViewSwitching/Plugin.cs
@@ -1,20 +1,52 @@
 ﻿using Renga;
 
-namespace SystemEvents
+namespace ViewSwitching
 {
 	public class Plugin : IPlugin
 	{
+		private readonly List<ActionEventSource> _eventSources = [];
+
+		private IApplication? _app;
+
 		public bool Initialize(string pluginFolder)
 		{
-			return true;
-		}
+			IApplication app = new Application();
+			_app = app;
+			IUI ui = app.UI;
 
-		private void OnProjectSaved(string obj)
-		{
+			IUIPanelExtension extension = ui.CreateUIPanelExtension();
+			extension.AddToolButton(CreateMainViewAction(ui));
+			ui.AddExtensionToPrimaryPanel(extension);
+
+			return true;
 		}
 
 		public void Stop()
 		{
+			foreach (ActionEventSource eventSource in _eventSources)
+				eventSource.Dispose();
+
+			_eventSources.Clear();
+		}
+
+		private IAction CreateMainViewAction(IUI ui)
+		{
+			IAction action = ui.CreateAction();
+			action.DisplayName = "MainActionView";
+
+			var events = new ActionEventSource(action);
+			events.Triggered += (sender, arguments) =>
+			{
+				if (_app is not null)
+				{
+					int buildingId = _app.Project.BuildingInfo.Id;
+					_app.OpenViewByEntity(buildingId);
+				}
+			};
+
+			_eventSources.Add(events);
+
+			return action;
 		}
 	}
 }
