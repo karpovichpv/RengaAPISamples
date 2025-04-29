a388be24d7858ec6dfe42a24bf1da863d73f591d
karpovichpv <karpovich.pavel@gmail.com>
2025-04-29 21:14:43 +0300

    Add the sample project

---
+++ plugins/VisualStylesAndVisibility/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using Renga;
+
+namespace VisualStylesAndVisibility
+{
+	public class Plugin : IPlugin
+	{
+		private readonly List<ActionEventSource> _eventSources = [];
+
+		private IApplication? _app;
+
+		public bool Initialize(string pluginFolder)
+		{
+			IApplication app = new Application();
+			_app = app;
+			IUI ui = app.UI;
+
+			IUIPanelExtension extension = ui.CreateUIPanelExtension();
+			ui.AddExtensionToPrimaryPanel(extension);
+
+			return true;
+		}
+
+		public void Stop()
+		{
+			foreach (ActionEventSource eventSource in _eventSources)
+				eventSource.Dispose();
+
+			_eventSources.Clear();
+		}
+	}
+}
