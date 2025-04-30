63f9541af8d2e6c16bf616ac94333f95d0bb6b00
karpovichpv <karpovich.pavel@gmail.com>
2025-04-30 20:59:56 +0300

    Export drawigns to pdf

--- applications/ExportDrawings/Program.cs
+++ applications/ExportDrawings/Program.cs
@@ -1,12 +1,38 @@
 ﻿
 using Renga;
+using System.Reflection;
 
-IApplication app = new Application();
-if (app is not null)
+public class Program
 {
-	Console.WriteLine($"app.ProductName: {app.ProductName}");
-}
-else
-{
-	Console.WriteLine($"connection failed");
+	public static void Main(string[] args)
+	{
+		IApplication application = new Application();
+		if (application is not null)
+		{
+			int result = application.OpenProject("c:\\other\\project.rnp");
+			IProject project = application.Project;
+			if (project is not null)
+			{
+				IDrawingCollection drawings = project.Drawings;
+				DirectoryInfo? directoryInfo = Directory.GetParent(Assembly.GetExecutingAssembly().Location);
+				string? path = null;
+				if (directoryInfo is not null)
+				{
+					path = directoryInfo.FullName;
+				}
+				if (path is null)
+				{
+					Console.WriteLine($"Cannot defined the program path");
+					return;
+				}
+
+				for (int i = 0; i < drawings.Count; i++)
+				{
+					IDrawing drawing = drawings.Get(i);
+					drawing.ExportToPdf(Path.Combine(path, drawing.Name), true);
+					Console.WriteLine($"Drawing {drawing.Name} was exported");
+				}
+			}
+		}
+	}
 }
