4d611e0a96565d0de44c924787d2cdaa13b8ee5c
karpovichpv <karpovich.pavel@gmail.com>
2025-04-30 20:31:20 +0300

    Add the test net8 console application

---
+++ applications/TestNet8Application/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Renga;
+
+IApplication app = new Application();
+if (app is not null)
+{
+	Console.WriteLine($"app.ProductName: {app.ProductName}");
+}
+else
+{
+	Console.WriteLine($"connection failed");
+}
