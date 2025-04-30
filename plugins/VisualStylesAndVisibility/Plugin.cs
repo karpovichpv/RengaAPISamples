cee5d0c81cf2ed66315717a32aabb97b54e1f5c3
karpovichpv <karpovich.pavel@gmail.com>
2025-04-30 20:15:35 +0300

    Change the current view style to color

--- plugins/VisualStylesAndVisibility/Plugin.cs
+++ plugins/VisualStylesAndVisibility/Plugin.cs
@@ -15,6 +15,7 @@ namespace VisualStylesAndVisibility
 			IUI ui = app.UI;
 
 			IUIPanelExtension extension = ui.CreateUIPanelExtension();
+			extension.AddToolButton(CreateChangeViewStyleAction(ui));
 			ui.AddExtensionToPrimaryPanel(extension);
 
 			return true;
@@ -26,6 +27,32 @@ namespace VisualStylesAndVisibility
 				eventSource.Dispose();
 
 			_eventSources.Clear();
+		}
+
+		private IAction CreateChangeViewStyleAction(IUI ui)
+		{
+			IAction action = ui.CreateAction();
+			action.ToolTip = "Change to the current view style to color";
+			ActionEventSource source = new(action);
+			source.Triggered += (sender, arguments) =>
+			{
+				if (_app is not null)
+				{
+					IView? view = _app.ActiveView;
+					if (view is IModelView modelView)
+					{
+						VisualStyle style = modelView.VisualStyle;
+						modelView.VisualStyle = VisualStyle.VisualStyle_Color;
+					}
+				}
+			};
+
+			return action;
+		}
+
+		private void Source_Triggered(object? sender, EventArgs e)
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
